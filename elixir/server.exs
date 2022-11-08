5444de11362e00ff9ac056ced84f4a95d6b6416e
Riccardo Manfrin <riccardo.manfrin@athonet.com>
2022-11-08 08:55:51 +0100

    Certificate UID handling

--- elixir/server.exs
+++ elixir/server.exs
@@ -1,15 +1,45 @@
 Mix.install([{:cowboy, "~> 2.9.0"}])
 
+defmodule Cert do
+  require Record
+  @certificate_def Record.extract(:Certificate, from_lib: "public_key/include/OTP-PUB-KEY.hrl") |> IO.inspect()
+  @tbs_certificate_def Record.extract(:TBSCertificate, from_lib: "public_key/include/OTP-PUB-KEY.hrl")
+  Record.defrecord(:certificate, :Certificate, @certificate_def)
+  Record.defrecord(:tbs_certificate, :TBSCertificate, @tbs_certificate_def)
+  def get_userid(certificate(tbsCertificate: tbs_certificate(subject: {:rdnSequence, atnv_list}))) do
+    res = for [{:AttributeTypeAndValue, {0, 9, 2342, 19200300, 100, 1, 1}, uid}] <- atnv_list, do: uid
+    <<12, _len, uid::binary>> = List.first(res)
+    #https://www.rfc-editor.org/rfc/rfc2798
+    # ( 0.9.2342.19200300.100.1.1
+    # NAME 'uid'
+    # EQUALITY caseIgnoreMatch
+    # SUBSTR caseIgnoreSubstringsMatch
+    # SYNTAX 1.3.6.1.4.1.1466.115.121.1.15{256} )
+    uid |> String.downcase
+  end
+  def get_userid(_), do: nil
+end
+
 certsPath = 'keys'
 
 defmodule Toppage do
-  def init(req, userctx) do
+  @authorized_users ["foo", "bar", "zoo", "mickey mouse", "anythinggoes"]
+  def init(req, _userctx) do
     #IO.inspect {req, userctx}
     client_cert = :cowboy_req.cert(req) #|> IO.inspect(limit: :infinity)
-    :public_key.pem_decode(client_cert) #|> IO.inspect()
-    req = :cowboy_req.reply(200, %{
-        "content-type" => "text/plain"
-      }, "Hello World!", req)
+    certificate  = :public_key.der_decode(:Certificate, client_cert)
+    user = Cert.get_userid(certificate)
+    req = if user in @authorized_users do
+        :cowboy_req.reply(200, %{
+          "content-type" => "text/plain"
+        }, "Hello World user #{user}! You are authorized", req)
+      else
+        :cowboy_req.reply(401, %{
+          "content-type" => "text/plain"
+        }, "Sorry user #{user}.. You are unauthorized", req)
+      end
+
+    req =
     {:ok, req, []}
   end
 end
@@ -34,10 +64,10 @@ name = :example
     verify: :verify_peer, #you need this for :cowboy_req.cert to not return :undefined
                           #https://ninenines.eu/docs/en/cowboy/2.9/manual/cowboy_req.cert/
     verify_fun: {fn
-        (_, {:bad_cert, :selfsigned_peer}, userState) -> {:fail, "No self signed certs allowed"}; # Allow self-signed certificates
+        (_, {:bad_cert, :selfsigned_peer}, _userState) -> {:fail, "No self signed certs allowed"}; # Allow self-signed certificates
         (_,{:bad_cert, _} = reason, _) -> {:fail, reason};
         (_,{:extension, _}, userState) -> {:unknown, userState};
-        (_, :valid, userState) -> {:fail, "Peer not validated"};
+        (_, :valid, _userState) -> {:fail, "Peer not validated"};
         (_, :valid_peer, userState) -> {:valid, userState}
       end, []}
   ],
